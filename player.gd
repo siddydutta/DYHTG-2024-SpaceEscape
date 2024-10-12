c646d43995667a72e3e0b8a4e08d8cf3cd221f62
Lukas Moers <luk.moers@gmail.com>
2024-10-12 17:22:26 +0100

    Added power up frames

--- player.gd
+++ player.gd
@@ -53,7 +53,7 @@ func _on_mob_collision(mob: Node2D) -> void:
 # Handle power-up collision (player collects a power-up)
 func _on_power_up_collision(power_up: Node2D) -> void:
 	# Increase the player's speed by 1.2x
-	speed_multiplier *= 5  # Multiply the current speed by 1.2
+	speed_multiplier *= 1.2  # Multiply the current speed by 1.2
 	power_up_collected.emit()  # Emit a signal for collecting the power-up
 
 	# You might want to hide or remove the power-up from the scene
--- powerup.gd
+++ powerup.gd
@@ -1,14 +1,17 @@
 extends RigidBody2D
 
+var screen_size # Size of the game window.
 
-# Called when the node enters the scene tree for the first time.
+var sprite = $AnimatedSprite2D
+
+
 func _ready():
-	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
-	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
+	sprite.play("powerup")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	$AnimatedSprite2D.animation = "powerup"
+	$AnimatedSprite2D.play()
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
