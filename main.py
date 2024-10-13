24245869163e7542c10c45fdac68187dd7a20c65
Siddhartha Dutta <siddharthadutta@protonmail.com>
2024-10-13 07:22:07 +0100

    single hand gesture control

--- main.py
+++ main.py
@@ -14,7 +14,22 @@ hands = mp_hands.Hands(
     model_complexity=1, 
     min_detection_confidence=0.75, 
     min_tracking_confidence=0.75, 
-    max_num_hands=2) 
+    max_num_hands=1) 
+
+
+def get_hand_position(hand_landmarks, img_width):
+    # x-coordinate of the middle finger tip (landmark 12)
+    x = hand_landmarks.landmark[12].x * img_width
+
+    center_start = img_width * 0.4
+    center_end = img_width * 0.6
+
+    if x < center_start:
+        return 'left'
+    elif x > center_end:
+        return 'right'
+    return 'center'
+
 
 cap = cv2.VideoCapture(0) 
 while True: 
@@ -22,14 +37,16 @@ while True:
     img = cv2.flip(img, 1) 
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
     results = hands.process(imgRGB) 
-    if results.multi_hand_landmarks: 
-        if len(results.multi_handedness) != 2: 
-            for i in results.multi_handedness: 
-                label = MessageToDict(i)['classification'][0]['label']
-                if label == 'Left': 
-                    client_socket.sendto(b'\x00', (server_ip, server_port))
-                if label == 'Right':
-                    client_socket.sendto(b'\x01', (server_ip, server_port))
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            label = get_hand_position(hand_landmarks, img.shape[1])
+            if label == 'left':
+                client_socket.sendto(b'\x00', (server_ip, server_port))
+            elif label == 'right':
+                client_socket.sendto(b'\x01', (server_ip, server_port))
+            else:
+                pass
+
     # cv2.imshow('Image', img) 
     if cv2.waitKey(1) & 0xff == ord('q'): 
         break
